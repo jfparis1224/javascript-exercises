96eb91b6ad894d058f6674ea0c34ea06d29841c4
niche1224 <jfparis1224@gmail.com>
2026-03-26 18:28:18 +0800

    Finish reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let arrNS = str.split("");
+  let revNS = arrNS.reverse();
+  return revNS.join("")
 };
 
 // Do not edit below this line
