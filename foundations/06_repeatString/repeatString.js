9dd07e2b689c56773ffb04fb87797bdb9b23ddc2
niche1224 <jfparis1224@gmail.com>
2026-03-26 18:13:59 +0800

    Finish repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+let string = '';
+if (n>=0){   for (let i=0;i<n;i++){
+    string += str
+}
+return string}
+return "ERROR"
 };
 
 // Do not edit below this line
