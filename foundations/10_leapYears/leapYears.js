fe55f032edc01c1de8fc1443ae64878163c13558
niche1224 <jfparis1224@gmail.com>
2026-03-27 15:29:00 +0800

    Finish leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(args) {
+if(args%4===0&&args%100!==0||args%400===0){
+    return true
+}
+return false
 };
 
 // Do not edit below this line
