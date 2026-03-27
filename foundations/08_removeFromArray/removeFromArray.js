ac981099b89775c0502cdd34befd81202025aad6
niche1224 <jfparis1224@gmail.com>
2026-03-27 13:33:50 +0800

    Finish removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+let conv = Array.from(arguments)
+let arr = conv[0];
+for (let i=0;i<arguments.length;i++){
+  while (arr.includes(conv[i])){
+    arr.splice(arr.indexOf(conv[i]),1)
+    }
+  }
+return arr
 };
 
 // Do not edit below this line
