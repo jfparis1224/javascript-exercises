d82603b3c35caab138fdf76c425bb8955cf2999b
niche1224 <jfparis1224@gmail.com>
2026-03-28 14:57:21 +0800

    Finish calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i=0;i<arr.length;i++){
+    sum+=arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mul = 1;
+  for (let i=0;i<arr.length;i++){
+    mul *= arr[i]
+  }
+  return mul
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n<0||!Number.isInteger(n)){
+    return NaN
+  }
+  let res = 1;
+  for (let i=2; i<=n;i++){
+    res *= i;
+  }
+  return res
 };
 
 // Do not edit below this line
