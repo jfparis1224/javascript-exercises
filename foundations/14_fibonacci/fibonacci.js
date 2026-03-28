f5dd4324a12d017a64410442f7f9da8bcf262512
niche1224 <jfparis1224@gmail.com>
2026-03-28 16:56:29 +0800

    Finish fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n){
+    let arr= [1,1];
+    let num = 0;
+    let na=0;
+    let nb=1;
+    let i=Number(n)
+    if(i>1){
+    while (i!==arr.length){
+    num=arr[na]+arr[nb];
+    arr.push(num);
+    na++;
+    nb++
+        }
+        return arr[arr.length-1]
+    }
+    if(n<0){
+    return "OOPS"
+    }
+    if(i===1){
+    return arr[1]}
+return 0
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
