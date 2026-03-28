b55a07799062178f861c1f916e598cb0265106eb
niche1224 <jfparis1224@gmail.com>
2026-03-28 14:29:34 +0800

    Finish tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+let cel= (f-32)*(5/9);
+let celR= Math.round(cel*10)/10;
+return celR
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+let fah = (c*1.8)+32;
+let fahR= Math.round(fah*10)/10;
+return fahR
 };
 
 // Do not edit below this line
