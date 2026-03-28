21ae763c259ec1b03dcd5bdfc57e96ac413466c3
niche1224 <jfparis1224@gmail.com>
2026-03-28 15:38:45 +0800

    Finish palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str){
+let rep = str.toLowerCase().replace(/\s/g,'')
+let arr = rep.split("");
+while (arr.includes(",")||arr.includes("!")||arr.includes(".")){
+  let pos=arr.indexOf(",");
+  let pos1=arr.indexOf("!");
+  let pos2=arr.indexOf(".");
+  if (arr.includes(",")){
+  arr.splice(pos,1);
+    }
+  if(arr.includes("!")){
+  arr.splice(pos1,1);
+    }
+  if(arr.includes(".")){
+  arr.splice(pos2,1);
+    }
+  }
+let arrJ= arr.join("");
+let rev=[...arr].reverse().join("");
+if (arrJ===rev){
+  return true
+  }
+  return false
+}
 // Do not edit below this line
 module.exports = palindromes;
